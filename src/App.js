895ab8a745c254909b3f3f9d95f53e74e1f52335
V-Sirenko <the.sirenko@gmail.com>
2021-06-26 17:00:43 +0300

    Added a API Object

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+
+function App() {
+
+    const API = {
+        key: '19225c065a3dc3c5738e29676a4bdf2d',
+        base: 'http://api.openweathermap.org/data/2.5/'
+    }
+    return (
+        <Fragment>
+
+
+
+        </Fragment>
+    );
+}
+
+export default App;
