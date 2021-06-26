9ad1e8c786a0a739c3f630f1ff229e960251749a
V-Sirenko <the.sirenko@gmail.com>
2021-06-26 17:19:39 +0300

    Added states & JSX input  for a Search City

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function App() {
 
@@ -6,10 +6,32 @@ function App() {
         key: '19225c065a3dc3c5738e29676a4bdf2d',
         base: 'http://api.openweathermap.org/data/2.5/'
     }
+    const [city, setCity] = useState('');
+    const [weather, setWeather] = useState({})
+
+    const search = evt => {
+        if (evt.key === 'Enter') {
+            fetch(`${API.base}weather?q=${city}&units=metric&appid=${API.key}`)
+            .then(res => res.json())
+            .then(result => {
+                setWeather(result)        
+                console.log(result)
+            })
+        }
+    }
+
+
+
     return (
         <Fragment>
 
-
+        <input
+            type='text'
+            placeholder='Поиск...'
+            onChange={e => setCity(e.target.value)}
+            value={city}
+            onKeyPress={search}
+          />
 
         </Fragment>
     );
